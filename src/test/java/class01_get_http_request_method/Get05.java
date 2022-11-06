bbcacb3ac738e2358aa71b9f091bd87c1a7cc984
ercument0377 <ercument0377@gmail.com>
2022-11-06 20:18:11 +0300

    "get"

--- src/test/java/class01_get_http_request_method/Get05.java
+++ src/test/java/class01_get_http_request_method/Get05.java
@@ -2,13 +2,15 @@ package class01_get_http_request_method;
 
 import base_url.JsonPlaceHolderBaseUrl;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+import static org.testng.AssertJUnit.assertTrue;
 
 import base_url.HerOkuAppBaseUrl;
-public class Get05 {
+public class Get05 extends HerOkuAppBaseUrl {
     /*
         Given
             https://restful-booker.herokuapp.com/booking?firstname=Dane&lastname=Dominguez
@@ -18,13 +20,44 @@ public class Get05 {
             Status code : 200
       And
           Data'lar arasinda ismi (firstname) “Dane” ve soyismi (lastname) “Dominguez” olan biri olmali
+
+     //   1.adım  url yi set et
+     //   2. adım beklenen datayı set et
+     //   3.adım: get request gonder ve get response al
+     //   4.adım assertion  yap
+
      */
-// Query Params spesifik parametreleri seçmek için kullanılır(orn: ?firstname=Dane&lastname=Dominguez
-    // Path Params ise resource (kaynagı) kucultmek için daraltmak için kullanırız
+    // Query Params spesifik parametreleri seçmek için kullanılır(orn: ?firstname=Dane&lastname=Dominguez
+    // Path Params ise resource (kaynagı) kucultmek için daraltmak için kullanırız ( /booking  kısmıdır)
     @Test
     public void get05(){
+        //   1.adım  url yi set et
+        spec.pathParam("first","booking").queryParams("firstname","Dane","lastname","Dominguez");
 
-        spec.pathParam("")
+
+
+        //   2. adım beklenen datayı set et
+
+
+        //   3.adım: get request gonder ve get response al
+        Response response = given().spec(spec).when().get("/{first}");
+        response.prettyPrint();
+
+        //   4.adım assertion  yap
+        response.then().assertThat().statusCode(200).contentType(ContentType.JSON).body("bookingid",hasItem(38));
+
+        response.then().assertThat().statusCode(200);
+        assertTrue(response.asString().contains("booking"));
+
+
+
+
+
+
+
+
+
+
     }
 }
 
---
+++ src/test/java/class01_get_http_request_method/Get06.java
@@ -0,0 +1,76 @@
+package class01_get_http_request_method;
+
+import base_url.HerOkuAppBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class Get06 extends HerOkuAppBaseUrl {
+
+    /*
+        Given
+            https://restful-booker.herokuapp.com/booking/560
+        When
+            Kullanici GET request gonderir => URL
+        Then
+            HTTP Status Code: 200
+        And
+            Response content type : “application/json”
+        And
+            Response body asagidaki gibi olmali;
+            {
+                            "firstname": "akifoo",
+                              "lastname": "okutucu",
+                              "totalprice": 335,
+                             "depositpaid": true,
+                             "bookingdates": {
+                                            "checkin": "2018-01-01",
+                                              "checkout": "2019-01-01"
+                },
+                "additionalneeds": "Breakfast"
+            }"
+            }
+
+         //   1.adım  url yi set et
+         //   2. adım beklenen datayı set et
+         //   3.adım: get request gonder ve get response al
+         //   4.adım assertion  yap
+     */
+
+    @Test
+    public void get06(){
+
+        //   1.adım  url yi set et
+        spec.pathParams("first","booking","second",560);
+
+        //   2. adım beklenen datayı set et
+
+
+        //   3.adım: get request gonder ve get response al
+       Response response = given().spec(spec).when().get("/{first}/{second}");
+       response.prettyPrint();
+
+        //   4.adım assertion  yap
+        //1.yol
+
+        response.then().assertThat().statusCode(200).contentType(ContentType.JSON).
+                body("firstname", equalTo("akifoo")).
+                body("lastname",equalTo("okutucu")).
+                body("totalprice",equalTo(335)).
+                body("depositpaid",equalTo(true)).
+                body("bookingdates.checkin",equalTo("2018-01-01")).
+                body("bookingdates.checkout",equalTo("2019-01-01")).
+                body("additionalneeds",equalTo("Breakfast"));
+
+        //2.yol
+
+
+    }
+
+
+
+}
+
